1b41e2edc8ae245db4ee97bb7e9b1a0bc2a52aa5
Cheva560 <59962541+Cheva560@users.noreply.github.com>
2020-01-23 17:51:43 +0100

    Action + Attack + Perte de HpIa + DeBug Victoire/Défaite

--- Chavlier_Legal_we_made_a_violent_game.c
+++ Chavlier_Legal_we_made_a_violent_game.c
@@ -8,8 +8,9 @@ int main () {
 
 	int nHpPlayer=150;
 	int nHpIa=150;
+	int nAction=0;
 	
-	//for(int i = 0; condition d'arret; i++){}
+	//for(int i = 0; condition de fonctionnement; i++){}
 	
 	for (int i = 0;i <= 70;i++){
 		
@@ -19,12 +20,36 @@ int main () {
 	
 	// OR = || 		AND = &&        NO = !
 	//OR vérification des 2 valeurs
-	//while(nHpIa >=0 || nHpPlayer >=0){
-		
-		//printf ("Choose your actions\n");
-		
-	//}
+	//Conditions de défaite : dead Players or IA
 	
+	while(nHpIa >0 && nHpPlayer >0){
+		
+		nAction = 0;
+		while(nAction != 1){
+		
+			printf ("Choose your actions\n");
+			printf (" 1) Attack\n");
+			
+			//scanf ("%d", &nomVar); ==> l'utilisateur tape qqch dans la variable "maVar"
+			scanf ("%d", &nAction);
+		
+		}
+		
+		//if(condition d'activation){}
+		
+		//Si (Players press 1 {alors nHpIa prend sa valeur actuelle -20 -=20}
+		
+		
+		
+		if (nAction == 1) {
+			
+			nHpIa-=50;
+			printf("texte %d texte", nHpIa);
+			
+		}
+		
+		
+	}
 
 
 
