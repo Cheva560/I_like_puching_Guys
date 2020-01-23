375a64a1335dedf230192f923c2c317cb2a80ef5
Cheva560 <59962541+Cheva560@users.noreply.github.com>
2020-01-23 17:03:10 +0100

    Screen_Clear
    70 \n

--- Chavlier_Legal_we_made_a_violent_game.c
+++ Chavlier_Legal_we_made_a_violent_game.c
@@ -2,8 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main { 
-	printf("ça me soale");
 
+
+int main () { 
+
+	int nHpPlayer=150;
+	int nHpIa=150;
+	
+	//for(int i = 0; condition d'arret; i++){}
+	
+	for (int i = 0;i <= 70;i++){
+		
+		printf ("\n");
+		
+	}
+	
+	// OR = || 		AND = &&        NO = !
+	//OR vérification des 2 valeurs
+	//while(nHpIa >=0 || nHpPlayer >=0){
+		
+		//printf ("Choose your actions\n");
+		
+	//}
+	
+
+
+
+
+
+
+	
 	return 0 ;
 }
